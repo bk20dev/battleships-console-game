a0b966b262f62b48c6af39451821e5f731f69436
Bartosz Bieniek <itsbk20@gmail.com>
2024-11-17 17:45:30 +0100

    Implement addition of two core::position-s

--- core/dimension.hpp
+++ core/dimension.hpp
@@ -5,7 +5,17 @@ namespace core
     struct position
     {
         int x, y;
+
+        position operator+(const position& other_position) const;
     };
+
+    inline position position::operator+(const position& other_position) const
+    {
+        return position{
+            .x = x + other_position.x,
+            .y = y + other_position.y
+        };
+    }
 
     struct size
     {
