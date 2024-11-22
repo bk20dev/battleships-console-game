e5cb132ac95bcdcf2541413bc6e376950a6aca08
Bartosz Bieniek <itsbk20@gmail.com>
2024-11-22 22:23:48 +0100

    Create method for setting label style

--- components/label.hpp
+++ components/label.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "../console/style.hpp"
 #include "../core/component.hpp"
 
 namespace components
@@ -8,8 +9,12 @@ namespace components
     {
         std::string text;
 
+        std::optional<console::style::style> style;
+
     public:
         label(int x, int y, const std::shared_ptr<console::console>& console, const std::string& text);
+
+        void set_style(const std::optional<console::style::style>& style);
 
         void paint() override;
     };
