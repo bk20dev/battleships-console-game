cf657b6e89bcd1c6315bc69a95677c1805f540e9
Bartosz Bieniek <itsbk20@gmail.com>
2024-11-22 22:22:03 +0100

    Implement text styling

---
+++ console/style.cpp
@@ -0,0 +1,74 @@
+#include "style.hpp"
+
+#include <string>
+#include <__format/format_functions.h>
+
+/**
+ * Returns a Control Sequence Introducer. Read more at https://en.wikipedia.org/wiki/ANSI_escape_code#Control_Sequence_Introducer_commands
+ * @param attribute The n part of CSI, e.g. 31.
+ * @param code The code part of CSI, e.g. m (SGR).
+ * @return The attribute preceded by ANSI escape sequence.
+ */
+std::string create_control_sequence(const std::string& attribute, const std::string& code)
+{
+    return std::format("\x1b[{}{}", attribute, code);
+}
+
+int console::style::get_foreground_color_code(const color& color)
+{
+    return color;
+}
+
+int console::style::get_background_color_code(const color& color)
+{
+    return get_foreground_color_code(color) + 10;
+}
+
+int console::style::get_effect_code(const effect& effect)
+{
+    switch (effect)
+    {
+    case BOLD:
+        return 1;
+    case FAINT:
+        return 2;
+    case ITALIC:
+        return 3;
+    case UNDERLINE:
+        return 4;
+    }
+
+    const int effect_code = effect;
+    throw std::runtime_error(std::format("Unknown text effect. (effect={})", effect_code));
+}
+
+std::string console::style::style::to_control_sequence() const
+{
+    std::string style_sequence = ";0";
+
+    // Foreground color
+    const int foreground_color_code = get_foreground_color_code(foreground_color);
+    style_sequence += std::format(";{}", foreground_color_code);
+
+    // Background color
+    const int background_color_code = get_background_color_code(background_color);
+    style_sequence += std::format(";{}", background_color_code);
+
+    // Text effects
+    for (const auto& effect : all_effects)
+    {
+        if (text_effect & effect)
+        {
+            const int effect_code = get_effect_code(effect);
+            style_sequence += std::format(";{}", effect_code);
+        }
+    }
+
+    return create_control_sequence(style_sequence, STYLE_CONTROL_SEQUENCE);
+}
+
+std::string console::style::style::apply_to_text(const std::string& text) const
+{
+    const std::string color_sequence = to_control_sequence();
+    return color_sequence + text;
+}
---
+++ console/style.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <array>
+#include <string>
+
+namespace console::style
+{
+    static constexpr std::string STYLE_CONTROL_SEQUENCE = "m";
+
+    enum color
+    {
+        DEFAULT = 39,
+        BLACK = 30,
+        RED = 31,
+        GREEN = 32,
+        YELLOW = 33,
+        BLUE = 34,
+        MAGENTA = 35,
+        CYAN = 36,
+        WHITE = 37,
+        BRIGHT_BLACK = 90,
+        BRIGHT_RED = 91,
+        BRIGHT_GREEN = 92,
+        BRIGHT_YELLOW = 93,
+        BRIGHT_BLUE = 94,
+        BRIGHT_MAGENTA = 95,
+        BRIGHT_CYAN = 96,
+        BRIGHT_WHITE = 97,
+    };
+
+    int get_foreground_color_code(const color& color);
+
+    int get_background_color_code(const color& color);
+
+    enum effect
+    {
+        BOLD = 1,
+        FAINT = 2,
+        ITALIC = 4,
+        UNDERLINE = 8,
+    };
+
+    constexpr std::array all_effects = {BOLD, FAINT, ITALIC, UNDERLINE};
+
+    int get_effect_code(const effect& effect);
+
+    struct style
+    {
+        color foreground_color = DEFAULT;
+        color background_color = DEFAULT;
+        int text_effect = 0;
+
+        [[nodiscard]] std::string to_control_sequence() const;
+
+        [[nodiscard]] std::string apply_to_text(const std::string& text) const;
+    };
+}
