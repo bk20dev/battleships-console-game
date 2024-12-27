ce9f21284245f4f0ac9aa5001208e83a7330a9c4
Bartosz Bieniek <itsbk20@gmail.com>
2024-12-27 16:35:50 +0100

    Create offset dimension

--- core/dimension.hpp
+++ core/dimension.hpp
@@ -4,9 +4,19 @@ namespace core
 {
     struct position
     {
-        int x, y;
+        int x = 0, y = 0;
 
         position operator+(const position& other_position) const;
+    };
+
+    struct offset
+    {
+        int x = 0, y = 0;
+    };
+
+    struct size
+    {
+        int width, height;
     };
 
     inline position position::operator+(const position& other_position) const
@@ -16,9 +26,4 @@ namespace core
             .y = y + other_position.y
         };
     }
-
-    struct size
-    {
-        int width, height;
-    };
 }
