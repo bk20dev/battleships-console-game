e42985eeef31dddf1fd50f6643a5ccc6d4cb1850
Bartosz Bieniek <itsbk20@gmail.com>
2025-01-02 01:19:00 +0100

    Create a constant with all available battleships

--- constants/constants.hpp
+++ constants/constants.hpp
@@ -4,4 +4,17 @@ namespace constants
 {
     constexpr int board_row_count = 10;
     constexpr int board_column_count = 10;
+
+    constexpr std::array battleships = {
+        models::battleship{.id = 0, .rectangle = {.size = {.height = 1, .width = 4}}},
+        models::battleship{.id = 1, .rectangle = {.size = {.height = 1, .width = 3}}},
+        models::battleship{.id = 2, .rectangle = {.size = {.height = 1, .width = 3}}},
+        models::battleship{.id = 3, .rectangle = {.size = {.height = 1, .width = 2}}},
+        models::battleship{.id = 4, .rectangle = {.size = {.height = 1, .width = 2}}},
+        models::battleship{.id = 5, .rectangle = {.size = {.height = 1, .width = 2}}},
+        models::battleship{.id = 6, .rectangle = {.size = {.height = 1, .width = 1}}},
+        models::battleship{.id = 7, .rectangle = {.size = {.height = 1, .width = 1}}},
+        models::battleship{.id = 8, .rectangle = {.size = {.height = 1, .width = 1}}},
+        models::battleship{.id = 9, .rectangle = {.size = {.height = 1, .width = 1}}},
+    };
 }
